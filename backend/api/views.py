58aa7bbe423f4e56a3414297e8f086e3e1fbeb70
piter <piter@piterdev.me>
2025-01-29 13:33:12 +0100

    Initial commit

---
+++ backend/api/views.py
@@ -0,0 +1,10 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def test_view(request):
+    data = {
+        'message': 'Hello, from the API!'
+    }
+    return Response(data)
+
